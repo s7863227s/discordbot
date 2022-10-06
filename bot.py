5c0cf0334679ca3feda253e23ac702359ae093fb
s7863227s <47858806+s7863227s@users.noreply.github.com>
2022-10-06 23:48:34 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,9 @@ intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
 from discord.ext import commands
+
 #client 是我們與 Discord 連結的橋樑
-bot = commands.Bot(command_prefix='[', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 #調用 event 函式庫
 @bot.event
@@ -13,5 +14,8 @@ bot = commands.Bot(command_prefix='[', intents=intents)
 async def on_ready():
     print("上線中")
     
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
 
 bot.run('MTAyNjQ2NzMwNDQ3NDY4MTQxNA.GbRJfT.W_DCfdEqdw0bpNIDCcQpXJk3MeqrnNr0CjfLdE') #TOKEN 在剛剛 Discord Developer 那邊「BOT」頁面裡面
