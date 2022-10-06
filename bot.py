7b509098e616109edbe553924f6726bed6b326b8
s7863227s <47858806+s7863227s@users.noreply.github.com>
2022-10-06 23:53:23 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,19 +3,15 @@ import discord
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
+intents.message_content = True
 from discord.ext import commands
 
 #client 是我們與 Discord 連結的橋樑
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-#調用 event 函式庫
-@bot.event
-#當機器人完成啟動時
-async def on_ready():
-    print("上線中")
     
 @bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
+async def test(ctx):
+    await ctx.send('pong')
 
-bot.run('MTAyNjQ2NzMwNDQ3NDY4MTQxNA.GbRJfT.W_DCfdEqdw0bpNIDCcQpXJk3MeqrnNr0CjfLdE') #TOKEN 在剛剛 Discord Developer 那邊「BOT」頁面裡面
+bot.run('MTAyNjQ2NzMwNDQ3NDY4MTQxNA.GGSyj-.i0myh6JxGRPw_ahJ7q2yhPbQoIoe2leR3UsU-8') #TOKEN 在剛剛 Discord Developer 那邊「BOT」頁面裡面
