95446bb000a1a3a9a99886d85d3d58b01f1b3a56
s7863227s <47858806+s7863227s@users.noreply.github.com>
2022-10-06 23:35:40 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,5 +13,19 @@ bot = commands.Bot(command_prefix='[', intents=intents)
 async def on_ready():
     print("上線中")
 
+#當有訊息時
+async def on_message(message):
+    #排除自己的訊息，避免陷入無限循環
+    if message.author == bot.user:
+        return
+    #如果以「說」開頭
+    if message.content.startswith('說'):
+      #分割訊息成兩份
+      tmp = message.content.split(" ",2)
+      #如果分割後串列長度只有1
+      if len(tmp) == 1:
+        await message.channel.send("你要我說什麼啦？")
+      else:
+        await message.channel.send(tmp[1])
 
 bot.run('MTAyNjQ2NzMwNDQ3NDY4MTQxNA.GbRJfT.W_DCfdEqdw0bpNIDCcQpXJk3MeqrnNr0CjfLdE') #TOKEN 在剛剛 Discord Developer 那邊「BOT」頁面裡面
